f6d01d1147305d05759b4dc8f9046510a61da398
Aaron Kessler <kessler.aaron@gmail.com>
2019-05-18 17:45:55 -0400

    remove state map code

--- 02_visualization.R
+++ 02_visualization.R
@@ -72,116 +72,12 @@ m1 <- leaflet(zip_map) %>%
              # fillColor = ~pal(cmag_d_spotcnt),
              label = lapply(labs1, HTML)
   ) %>%
-  addControl("Individual contributions by zip code (Q1)", position = "topright") 
+  addControl("Individual donations (itemized) by zip code (Q1)", position = "topright") 
 # %>% 
 #   setView(-96, 37.8, zoom=4) 
 
 m1
 
 #save to frameable file for CMS
-
 # htmlwidgets::saveWidget(frameableWidget(m1),'contribs_byzip_points.html')
-
-
-
-#.........................................
-#### MAPPING STATES ######################
-#.........................................
-
-
-
-
-#pull geo from tigris
-#for national map, 20m likely better here. For single states, 5m:
-
-# cd <- congressional_districts(cb = TRUE, resolution = "20m")
-state <- states(cb = TRUE, resolution = "20m")
-
-#"simplify" file to reduce file size
-# state <- ms_simplify(state, keep = 0.1)
-state
-
-#remove AK and HI
-mymap <- mymap %>% 
-  filter(state.name != "Alaska",
-         state.name != "Hawaii")
-
-
-mymap <- state
-### TMAP ####
-
-mymap_test <-  tm_shape(mymap) +
-  tm_polygons("ALAND", id = "STUSPS") 
-
-mymap_test
-
-
-
-
-#### LEAFLET ####
-
-
-#create test map layer
-
-leaflet(mymap) %>% 
-  addTiles() %>% 
-  addPolygons(color = "#444444") %>%
-  setView(-96, 37.8, zoom=4)
-
-
-### mapping ####
-
-#palette
-pal <- colorNumeric(
-  palette = "Greens",
-  domain = mymap$ALAND)
-
-#labels
-labs <- lapply(seq(nrow(mymap)), function(i) {
-  paste0( '<p><strong>', mymap[i, "house_dist"], '</strong></p>',
-          '<p></p>',
-          "Pct with 4-year degree: ", mymap[i, "pct.ed.college.all"]
-          # # '<p></p>', 
-          # districts_wpoints[i, "cmag_r_spotcnt"]
-  ) 
-})
-
-labs$Value[1]
-
-#make map
-leaflet(mymap) %>%
-  addProviderTiles(providers$CartoDB.Positron) %>%
-  addPolygons(color = "#444444", weight = .5, smoothFactor = 0.5,
-              opacity = 1.0, fillOpacity = 0.9,
-              fillColor = ~pal(pct.ed.college.all),
-              label = lapply(labs, HTML),
-              highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                  bringToFront = TRUE)) %>%
-  addLegend("bottomright", pal = pal, values = ~pct.ed.college.all,
-            title = "Pct with Bachelor's or Higher",
-            opacity = 1) 
-
-
-
-#make map - key races
-pal <- colorFactor(
-  palette = c("#6284d9","#e76d6f","#4152ad","#b64f49","#3f842b"),
-  domain = mymap$keyrace_rating)
-
-leaflet(mymap) %>%
-  addProviderTiles(providers$CartoDB.Positron) %>%
-  addPolygons(color = "#444444", weight = .5, smoothFactor = 0.5,
-              opacity = 1.0, fillOpacity = 0.9,
-              fillColor = ~pal(keyrace_rating),
-              highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                  bringToFront = TRUE)) %>%
-  addLegend("bottomright", pal = pal, values = ~keyrace_rating,
-            title = "Key Race Ratings",
-            opacity = 1) %>% 
-  setView(-96, 37.8, zoom=4)
-
-
-
-
-
 
