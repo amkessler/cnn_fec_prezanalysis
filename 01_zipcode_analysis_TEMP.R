fa446e998c46375671e707a3497912b83b104658
Aaron Kessler <kessler.aaron@gmail.com>
2019-05-23 23:11:53 -0400

    add temp files for backup work

---
+++ 01_zipcode_analysis_TEMP.R
@@ -0,0 +1,227 @@
+#first we'll run script step 00 to connect to db
+source("00_connecttodb.R")
+
+library(tidyverse)
+library(lubridate)
+library(janitor)
+library(dbplyr)
+
+#pull in the schedule A table from postgres db
+contribs_db <- tbl(con, "cycle_2020_schedulea")
+
+glimpse(contribs_db)
+
+#filter out only individual contributions, and active ones
+#create zip5 field by pulling out just first five digits
+contribs_db <- contribs_db %>% 
+  filter(status == "ACTIVE",
+         entity_type == "IND") %>% 
+  mutate(
+    zip5 = str_sub(str_trim(contributor_zip), 1, 5)
+  )
+
+#group by zip
+contribs_db %>% 
+  group_by(filer_committee_id_number, zip5) %>% 
+  summarise(sumcontribs = sum(contribution_amount)) %>% 
+  arrange(desc(sumcontribs))
+
+#collect into local dataframe for joining
+#group by zip
+by_zip_and_filer <- contribs_db %>% 
+  group_by(filer_committee_id_number, zip5) %>% 
+  summarise(sumcontribs = sum(contribution_amount)) %>% 
+  arrange(desc(sumcontribs)) %>% 
+  ungroup() %>% 
+  collect()
+
+
+
+##### BRING IN CANDIDATE TABLE TO JOIN ####
+
+## get the associated name from fec cmte id
+## we'll also limit to just Prez cmtes
+
+#pull candidate table from postgres db
+cand_db <- tbl(con, "cycle_2020_candidate")
+
+#filter only for presidential
+candnames <- cand_db %>% 
+  filter(district == "US") %>% 
+  select(name, fec_committee_id) %>% 
+  collect()
+
+#join
+contribs_by_zip <- inner_join(by_zip_and_filer, candnames, by = c("filer_committee_id_number" = "fec_committee_id"))
+
+#see if there are any missing zips and save for review
+zip_missing <- contribs_by_zip %>% 
+  filter(is.na(zip5))
+
+#reorder columns, arrange
+contribs_by_zip <- contribs_by_zip %>% 
+  filter(!is.na(zip5)) %>% 
+  select(name, everything()) %>% 
+  arrange(name, desc(sumcontribs))
+
+#any missing zips?
+contribs_by_zip %>% 
+  filter(is.na(zip5))
+
+#any repeated zips?
+contribs_by_zip %>% 
+  count(name, zip5) %>% 
+  filter(n > 1)
+
+
+
+#### BRING IN ZIP CODE LOOKUP TABLE #####
+
+## we'll add named location associated with each zip code
+
+ziplookup_raw <- read_csv("zip-codes-database-STANDARD.csv", 
+                          col_types = cols(StateFIPS = col_character()))
+
+# keep only one distinct record per zip code
+ziplookup <- ziplookup_raw %>% 
+  clean_names() %>% 
+  select(zip_code, city, state, county, state_fips, county_fips, latitude, longitude) %>% 
+  distinct(zip_code, .keep_all = TRUE) 
+
+#any repeated zips?
+ziplookup %>% 
+  count(zip_code, state) %>% 
+  filter(n > 1)
+
+ziplookup %>% 
+  filter(zip_code == "01062") 
+
+
+# join 
+joined <- left_join(contribs_by_zip, ziplookup, by = c("zip5" = "zip_code"))
+
+#create column for just last name of candidate
+joined$lastname <- str_split(joined$name, ",", simplify = TRUE)[,1]
+
+#final table
+byzip_bycand <- joined %>% 
+  ungroup %>% 
+  select(lastname, everything(), -name) %>% 
+  mutate(fips = paste0(state_fips, county_fips)) 
+
+byzip_bycand
+
+#write to file
+write_csv(byzip_bycand, "output/byzip_bycand.csv")
+
+
+
+#### TOP 10 ZIPS FOR EACH CANDIDATE ####
+
+#group by zip
+top10_byzip_bycand <- byzip_bycand %>% 
+  group_by(lastname) %>% 
+  top_n(n = 10, wt = sumcontribs) %>% #pulls top 10 by sumcontribs value
+  ungroup()
+
+top10_byzip_bycand
+
+#write to file
+write_csv(top10_byzip_bycand, "output/top10_byzip_bycand.csv")
+
+#any common zips?
+top10_byzip_bycand %>% 
+  count(zip5, city) %>% 
+  arrange(desc(n))
+  
+
+
+# reshape to wide format as an alternative table structure ####
+test <- byzip_bycand %>% 
+  select(lastname, zip5, sumcontribs)
+
+test_wide <- test %>% 
+  spread(lastname, sumcontribs)
+
+byzip_bycand_wide <- test_wide
+
+#write to file
+write_csv(byzip_bycand_wide, "output/byzip_bycand_wide.csv")
+
+
+
+#### CALCULATING COUNTY-LEVEL TOTALS BASED ON ZIPS ####
+## For Magic Wall
+
+#start with existing zip breakdowns
+byzip_bycand
+
+#group by candidate, county
+bycounty_bycand <- byzip_bycand %>% 
+  filter(!is.na(county)) %>% 
+  group_by(lastname, fips, county, state) %>% 
+  summarise(sum_in_county = sum(sumcontribs)) 
+
+#any repeated fips?
+bycounty_bycand %>% 
+  count(lastname, fips) %>% 
+  filter(n > 1)
+
+#write to file
+write_csv(bycounty_bycand, "output/bycounty_bycand.csv")
+
+
+#reshaped version to wide
+test_c <- bycounty_bycand %>%
+  select(lastname, fips, sum_in_county)
+
+test_c_wide <- test_c %>%
+  spread(lastname, sum_in_county)
+
+bycounty_bycand_wide <- test_c_wide
+
+#write to file
+write_csv(bycounty_bycand_wide, "output/bycounty_bycand_wide.csv")
+
+
+
+#### COMPARING TWO DIFFERENT CANDIDATES' ZIP CODE PERFORMANCE ####
+
+# select first candidate
+cand1 <- "Buttigieg"
+
+z_cand1 <- byzip_bycand %>% 
+  filter(lastname == cand1) %>% 
+  select(zip5, cand1_contribs = sumcontribs)
+  
+# select second candidate
+cand2 <- "Warren"
+
+z_cand2 <- byzip_bycand %>% 
+  filter(lastname == cand2) %>% 
+  select(zip5, cand2_contribs = sumcontribs)  
+
+
+### join to compare cand1 and cand2 in each zip
+zipcompare <- full_join(z_cand1, z_cand2)
+
+#change NAs to 0
+zipcompare <- zipcompare %>% 
+  replace(., is.na(.), 0)
+
+#calculate winner and money spread
+zipcompare <- zipcompare %>% 
+  mutate(
+    winner = ifelse(cand1_contribs>cand2_contribs, cand1, cand2),
+    advantage = abs(cand1_contribs-cand2_contribs) 
+  ) %>% 
+  rename(!!cand1:=cand1_contribs, #this special coding pulls the variable name into the rename function
+         !!cand2:=cand2_contribs) 
+
+#join with zip lookup table
+joined_temp <- left_join(zipcompare, ziplookup, by = c("zip5" = "zip_code"))
+zipcompare <- joined_temp
+
+#save to file
+saveRDS(zipcompare, "zipcompare.rds")
+
---
+++ 02_zipcode_visualization_TEMP.R
@@ -0,0 +1,200 @@
+source("01_zipcode_analysis_TEMP.R")
+
+library(tidyverse)
+library(janitor)
+library(tidycensus)
+library(tigris)
+library(sf)
+library(leaflet)
+library(tmap)
+library(tmaptools)
+library(RColorBrewer)
+library(htmlwidgets)
+library(htmltools)
+library(widgetframe)
+library(sp)
+library(raster)
+library(mapview)
+library(scales)
+library(viridis)
+options(tigris_class = "sf")
+
+
+### ZIP CODE MAPPING ####
+
+#checking to make sure zip results loaded
+byzip_bycand
+
+#filter out non-continental US 
+byzip_bycand %>% 
+  count(state)
+
+us <- unique(fips_codes$state)[1:51]
+
+zip_map <- byzip_bycand %>% 
+  filter(state %in% us)
+
+zip_map <- zip_map %>% 
+  filter(state !="HI",
+         state !="AK",
+         !is.na(state))
+  
+zip_map %>% 
+  count(state)
+
+
+  
+#pull out just a single candidate
+zip_map %>% 
+  count(lastname)
+
+zip_map <- zip_map %>% 
+  filter(lastname == "Sanders")
+
+
+#### MAPPING POINTS #####
+
+#labels
+labs1 <- lapply(seq(nrow(zip_map)), function(i) {
+  paste0( '<p>', 'Zip code: ', '<strong>', zip_map[i, "zip5"], '</strong></p>',
+          '<p></p>', 
+          "1st quarter donations: $", zip_map[i, "sumcontribs"]
+  ) 
+})
+
+m1 <- leaflet(zip_map) %>% 
+  # addTiles() %>%
+  addProviderTiles(providers$Stamen.TonerLite) %>% #alternative tile background
+  addCircles(lng = ~longitude, 
+             lat = ~latitude, 
+             weight = 1,
+             # stroke = FALSE,
+             # fillOpacity = .50,
+             radius = ~sqrt(sumcontribs) * 300, 
+             # fillColor = ~pal(cmag_d_spotcnt),
+             label = lapply(labs1, HTML)
+  ) %>%
+  addControl("Individual donations (itemized) by zip code", position = "topright") %>% 
+  setView(-96, 37.8, zoom=4)
+
+
+m1
+
+#save to frameable file for CMS
+htmlwidgets::saveWidget(frameableWidget(m1),'candidate_byzip.html')
+
+
+
+
+
+#.................................................................
+#### COMPARING TWO DIFFERENT CANDIDATES' ZIP CODE PERFORMANCE ####
+
+#check zipcompare file from step 01
+zipcompare
+
+#give it a new name for this process
+zipcompare_map <- zipcompare
+
+zipcompare_map$GEOID <- zipcompare_map$zip5
+
+#remove any negative values 
+zipcompare_map <- zipcompare_map %>% 
+  filter(!!cand1 >= 0,
+         !!cand2 >= 0)
+
+# zipcompare <- zipcompare %>% 
+#   filter(!str_detect(GEOID, "^99"),
+#          !str_detect(GEOID, "^96"),
+#          !str_detect(GEOID, "^006"),
+#          !str_detect(GEOID, "^007"),
+#          !str_detect(GEOID, "^009")
+#   )
+
+# #add dollar formatting
+# zip_map$demdisplay <- dollar(zip_map$demtotal)
+# zip_map$gopdisplay <- dollar(zip_map$goptotal)
+
+# color palette for map
+zipcompare_map$winner_text <- zipcompare_map$winner
+zipcompare_map$winner <- as.factor(zipcompare_map$winner)
+factpal <- colorFactor(c("green","orange"), zipcompare_map$winner)
+
+#labels
+labs2 <- lapply(seq(nrow(zipcompare_map)), function(i) {
+  paste0( '<p>', 'Zip code: ', '<strong>', zipcompare_map[i, "GEOID"], '</strong></p>',
+          '<p></p>', 
+          "Winner ", zipcompare_map[i, "winner_text"],
+          '<p></p>', 
+          "Advantage in Dollars: $", zipcompare_map[i, "advantage"]
+  ) 
+})
+
+m2 <- leaflet(zipcompare_map) %>% 
+  # addTiles() %>%
+  addProviderTiles(providers$Stamen.TonerLite) %>% 
+  addCircles(lng = ~longitude, 
+             lat = ~latitude, 
+             weight = .4,
+             stroke = FALSE,
+             fillOpacity = .45,
+             radius = ~sqrt(advantage) * 400, 
+             fillColor = ~factpal(winner),
+             label = lapply(labs2, HTML)
+  ) %>%
+  addControl("Individual donations (itemized) by zip code", position = "topright") %>% 
+  setView(-96, 37.8, zoom=4)
+
+m2
+
+#save to frameable file for CMS
+htmlwidgets::saveWidget(frameableWidget(m2),'cand_zipcompare.html')
+
+
+#### COMPARISON MAP OF ONLY NEW YORK CITY ####
+
+m2_nyc <- leaflet(zipcompare_map) %>% 
+  # addTiles() %>%
+  addProviderTiles(providers$Stamen.TonerLite) %>% 
+  addCircles(lng = ~longitude, 
+             lat = ~latitude, 
+             weight = .4,
+             stroke = FALSE,
+             fillOpacity = .45,
+             radius = ~sqrt(advantage) * 3, 
+             fillColor = ~factpal(winner),
+             label = lapply(labs2, HTML)
+  ) %>%
+  addControl("Individual donations (itemized) by zip code", position = "topright") %>% 
+  setView(-73.9840, 40.7549, zoom=12)
+
+m2_nyc
+
+#save to frameable file for CMS
+htmlwidgets::saveWidget(frameableWidget(m2_nyc),'cand_zipcompare_nyc.html')
+
+
+
+
+#### COMPARISON MAP OF ONLY VEGAS ####
+
+m2_vegas <- leaflet(zipcompare_map) %>%
+  # addTiles() %>%
+  addProviderTiles(providers$Stamen.TonerLite) %>%
+  addCircles(lng = ~longitude,
+             lat = ~latitude,
+             weight = .4,
+             stroke = FALSE,
+             fillOpacity = .45,
+             radius = ~sqrt(advantage) * 60,
+             fillColor = ~factpal(winner),
+             label = lapply(labs2, HTML)
+  ) %>%
+  addControl("Individual donations (itemized) by zip code", position = "topright") %>%
+  setView(-115.1398, 36.1699, zoom=12)
+
+m2_vegas
+
+#save to frameable file for CMS
+htmlwidgets::saveWidget(frameableWidget(m2_vegas),'cand_zipcompare_vegas.html')
+
