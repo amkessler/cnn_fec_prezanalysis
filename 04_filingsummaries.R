c42e94b1bd687f1ba677181403f5d77b3c267765
Aaron Kessler <kessler.aaron@gmail.com>
2019-06-03 23:11:17 -0400

    add script for pulling summaries from 2020 filing table

---
+++ 04_filingsummaries.R
@@ -0,0 +1,31 @@
+#first we'll run script step 00 to connect to db
+source("00_connecttodb.R")
+
+library(tidyverse)
+library(lubridate)
+library(writexl)
+library(dbplyr)
+
+#pull in the schedule A table from postgres db
+data <- tbl(con, "cycle_2020_filing")
+
+#look at the list of columns
+glimpse(data)
+
+#show the top handful of records
+head(data)
+
+
+#filter out only active presidential filings and download to LOCAL dataframe
+prezdata <- data %>% 
+  filter(active==TRUE,
+         status == "ACTIVE",
+         form == "F3P") %>% 
+  collect() #this is what downloads the slice of the data locally
+
+#now let's open it in the viewer to see what we have
+prezdata %>% 
+  View()
+
+#export our results to an Excel file 
+write_xlsx(prezdata, "prezdata_summaries.xlsx") #this file show up in your existing folder
