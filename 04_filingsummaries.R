0fbf963cfec4a7235eaec89b8451e619dc813a6c
Aaron Kessler <kessler.aaron@gmail.com>
2019-06-23 17:46:04 -0400

    convert date fields to date format

--- 04_filingsummaries.R
+++ 04_filingsummaries.R
@@ -24,6 +24,12 @@ prezdata <- data %>%
          form == "F3P") %>% 
   collect() #this is what downloads the slice of the data locally
 
+#convert several date fields to date format
+prezdata$coverage_from_date <- ymd(prezdata$coverage_from_date)
+prezdata$coverage_through_date <- ymd(prezdata$coverage_through_date)
+prezdata$election_date <- ymd(prezdata$election_date)
+prezdata$date_signed <- ymd(prezdata$date_signed)
+
 #now let's open it in the viewer to see what we have
 prezdata %>% 
   View()
